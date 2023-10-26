97bc3217412dc87c49623854ab3fe3c3a1ce5345
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-26 15:15:54 +0300

    Create 2_Learning.java

---
+++ 2_Learning/2_Learning.java
@@ -0,0 +1,60 @@
+**Study plan for electronic manufacturing process**
+
+**Areas to study:**
+
+* **Electronics fundamentals:** This includes topics such as circuit theory, semiconductor devices, and digital logic.
+* **PCB design:** This includes topics such as PCB layout, routing, and design for manufacturability.
+* **Component sourcing:** This includes topics such as component selection, supplier qualification, and inventory management.
+* **Assembly:** This includes topics such as pick-and-place, reflow soldering, and testing.
+* **Manufacturing processes:** This includes topics such as PCB fabrication, surface mount technology (SMT), and injection molding.
+* **Quality control:** This includes topics such as inspection, testing, and process improvement.
+
+**Resources and books:**
+
+* **Electronics Fundamentals: Circuits, Devices, and Applications** by Thomas L. Floyd
+* **PCB Design: A Practical Guide** by Chris Johnson
+* **Printed Circuit Boards: Design, Fabrication, and Assembly** by R.S. Khandpur
+* **Surface Mount Technology: Principles and Practice** by Ray Prasad
+* **Electronic Manufacturing Processes** by Glenn R. Blackwell
+
+**Softwares to use:**
+
+* **CAD software for PCB design:** Examples include Altium Designer, KiCad, and Eagle
+* **EDA software for circuit simulation:** Examples include LTspice and PSpice
+* **Pick-and-place machine software:** Examples include Yamaha YCP and Siemens Siplace
+
+**People to follow:**
+
+* **Ken Shirriff:** Ken is a hardware engineer and hacker who writes about electronics and manufacturing on his blog, Ken's Tech Blog.
+* **Dave Jones:** Dave is an electronics engineer and YouTuber who runs the channel EEVBlog.
+* **Chris Johnson:** Chris is a PCB designer and author of the book PCB Design: A Practical Guide.
+* **Ray Prasad:** Ray is a manufacturing engineer and author of the book Surface Mount Technology: Principles and Practice.
+
+**Articles and academic research to study:**
+
+* **A Review of Electronic Manufacturing Processes** by Glenn R. Blackwell
+* **The Future of Electronic Manufacturing** by IEEE Spectrum
+* **Trends in Electronic Manufacturing** by Gartner
+* **Electronic Manufacturing Process Optimization** by Elsevier
+
+**Topics to cover to become competent in electronic manufacturing process:**
+
+* **PCB design:** The ability to design a PCB from scratch, including layout, routing, and design for manufacturability.
+* **Component sourcing:** The ability to select the right components for a design, source them from reputable suppliers, and manage inventory.
+* **Assembly:** The ability to assemble electronic components onto a PCB, including pick-and-place, reflow soldering, and testing.
+* **Manufacturing processes:** The ability to understand and implement different electronic manufacturing processes, such as PCB fabrication, SMT, and injection molding.
+* **Quality control:** The ability to develop and implement quality control procedures to ensure that electronic products meet customer requirements.
+
+**Tips for becoming competent in electronic manufacturing process:**
+
+* **Get hands-on experience.** The best way to learn about electronic manufacturing is by doing it yourself. There are many kits and resources available to help you get started.
+* **Take courses and workshops.** There are many courses and workshops available on electronic manufacturing. These courses can teach you the fundamentals of PCB design, assembly, and quality control.
+* **Join a community.** There are many online and offline communities for electronic makers and hobbyists. Joining a community can help you learn from others and get help when you need it.
+* **Stay up-to-date on the latest trends and technologies.** The electronic manufacturing industry is constantly evolving. It is important to stay up-to-date on the latest trends and technologies to remain competitive.
+
+**Conclusion**
+
+Becoming competent in the electronic manufacturing process takes time and effort. However, by following the tips above, you can learn the skills and knowledge you need to succeed in this industry.
+
+
+  ,,,,
