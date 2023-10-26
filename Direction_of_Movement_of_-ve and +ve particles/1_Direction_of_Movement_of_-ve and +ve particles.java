cd685a1e178cd5ae8d9648bbb73437a93b34458e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-26 21:40:58 +0300

    Create 1_Direction_of_Movement_of_-ve and +ve particles.java

---
+++ Direction_of_Movement_of_-ve and +ve particles/1_Direction_of_Movement_of_-ve and +ve particles.java
@@ -0,0 +1,25 @@
+
+in electricy , does -ve chagrge mov e towards positive or do  +ve charges move towards -ve,
+
+
+
+
+
+
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+Opposite charges attract, so negative charges move towards positive charges and positive charges move towards negative charges. This is the basic principle of electricity.
+
+When you connect a wire to the positive and negative terminals of a battery, the electrons in the wire are attracted to the positive terminal and repelled by the negative terminal. This causes the electrons to flow from the negative terminal to the positive terminal, creating an electric current.
+
+The electric current flows through the wire and through the circuit, powering the devices that are connected to it. The current continues to flow as long as the circuit is closed and the battery is not dead.
+
+Here is a simple analogy:
+
+Imagine a magnet with a north pole and a south pole. If you bring two magnets together, the north pole of one magnet will attract the south pole of the other magnet. This is because opposite poles attract.
+
+The same is true for electric charges. Opposite charges attract. This is why negative charges move towards positive charges and positive charges move towards negative charges.
+
+I hope this helps!
